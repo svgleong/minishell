28f64268473556d7b40ec800c07eb8b47a2ffebf
koska <angelo.miguel.of@gmail.com>
2023-10-02 11:36:27 +0100

    one main only
    one main only

--- srcs/main.c
+++ srcs/main.c
@@ -15,6 +15,51 @@
 # include <readline/history.h> 
 # include <stdio.h>
 
+
+#include <executer.h>
+
+t_env	*env(void)
+{
+	static t_env env;
+	return (&env);
+}
+
+t_export_env	*export_env(void)
+{
+	static t_export_env export_env;
+	return (&export_env);
+}
+
+void	print_args(t_cmdd *cmd)
+{
+	printf("Debug Args: ");
+    for (int i = 0; cmd->args[i] != NULL; i++) {
+        printf("%s ", cmd->args[i]);
+    }
+	printf("\n");
+}
+
+
+int main(int ac, char **av, char **env)
+{
+    (void)ac;
+	(void)av;
+	t_cmdd cmd = {
+		.args = (char *[]){"echo", "-nnnnnnnni", "ola", NULL}
+	};
+	print_args(&cmd);
+	//pwd_bi();
+
+	
+	t_env *env_list;
+    env_list = NULL;
+	env_bi(env, env_list);
+	//list_bubble_sort(env);
+
+
+	return (0);
+}
+
 /* int main(int ac, char **av, char **env)
 {
 	(void)ac;
