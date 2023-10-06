79a6ece901f0ac242c8b236aebab6897e5284230
Sofia Valente Goncalves Leong <svalente@c2r5s4.42lisboa.com>
2023-10-06 13:00:31 +0100

    expansion almost done

--- srcs/expansion/expansion.c
+++ srcs/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: svalente <svalente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:02:23 by svalente          #+#    #+#             */
-/*   Updated: 2023/10/04 18:49:09 by svalente         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:57:39 by svalente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*get_status(char *str, int i)
 
 int	calculate_result(char *str , char *value, int key)
 {
+	if (!value)
+		return (ft_strlen_special(str, '$') + ft_strlen(ft_strchr(str, '$') + key));
 	return (ft_strlen_special(str, '$') + ft_strlen(value) + \
 	ft_strlen(ft_strchr(str, '$') + key));
 }
@@ -30,24 +32,56 @@ char	*expanding(char *str , char *value, int key)
 	char	*result;
 	int		sz;
 	int		i;
-	int		j;
 	int		k;
 
 	sz = calculate_result(str, value, key);
 	//printf("sz: %d\n", sz);
 	result = malloc(sz);
 	i = -1;
-	j = -1;
+	sz = -1;
 	while (str[++i] != '$')
 		result[i] = str[i];
 	k = i  + key + 1;
-	while (value[++j])
-		result[i++] = value[j];
+	while (value[++sz])
+		result[i++] = value[sz];
 	while (str[k])
 		result[i++] = str[k++];
 	result[i] = '\0';
 	printf("result %s\n", result);
-	return (result);	
+	return (result);
+}
+
+int	key_value(char *str)
+{
+	int	sz;
+
+	sz = 0;
+	while (str[++sz] && ft_isalnum(str[sz]))
+		;
+	return (sz -1);
+}
+
+char	*remove_expand(char *str)
+{
+	char	*new_str;
+	int		sz;
+	int		key;
+	int		i;
+	
+	key = key_value(ft_strchr(str, '$'));
+	sz = calculate_result(str, NULL, key);
+	new_str = malloc(sz);
+	sz = -1;
+	while (str[++sz] && str[sz] != '$')
+		new_str[sz] = str[sz];
+	i = sz + key + 1;
+	while (str[i])
+	{
+		new_str[sz++] = str[i];
+		i++;
+	}
+	new_str[sz] = '\0';
+	return (new_str);
 }
 
 char	*expansion(char *str, int i)
@@ -57,6 +91,9 @@ char	*expansion(char *str, int i)
 
 	env = data()->envp;
 	value = NULL;
+
+	if (!str[i + 1])
+		return "\2";
 	if (str[i + 1] == '?')
 		return ((str = get_status(str, i)));
 	while (env)
@@ -68,18 +105,10 @@ char	*expansion(char *str, int i)
 		}
 		env = env->next;
 	}
-	// remove_expand();
-
-	// while (str[++i])
-	// {
-	// 	if (!ft_isalpha(str[i]) || str[i] != '_')
-			
-	// 	//apagar $ e numero
-	// 	//se nao existe apagar ate $
-	// 	//se existir copiar a partir do igual
-	// 	//se encontrar ' depois de $ nao expande echo "$'PATH'" --> $'PATH'
-	// }
-	return (str);
+	if (!ft_isalnum(str[i + 1]))
+		return "\2";
+	value = remove_expand(str);
+	return (value);
 }
 
 int	search_expansion(t_cmd *cmds)
@@ -101,11 +130,14 @@ int	search_expansion(t_cmd *cmds)
 			else if (cmds->args[i][j] && cmds->args[i][j] == '$')
 			{
 				tmp = expansion(cmds->args[i], j);
+				if (tmp[0] == '\2')
+					continue ;
 				free(cmds->args[i]);
 				cmds->args[i] = tmp;
+				//printf("cmd: %s\n", cmds->args[i]);
 				j = -1;
 				//(void)tmp;
-				// $+USER ou $$USER ou $'USER'
+				// $+USER ou !!!!!$$USER ou $'USER'
 			}
 		}
 	}
